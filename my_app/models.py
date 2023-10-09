4dcde3db748cd2eee6994fd7b0905e5a95482162
Vitor Otsuka <vitor-ots@hotmail.com>
2023-10-09 20:48:43 -0300

    Continuação

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+#Django ORM - Object Relational Mapper - Banco de dados
+class Post(models.Model):
+    
+    title = models.CharField(max_length=255)
+    content = models.TextField()
