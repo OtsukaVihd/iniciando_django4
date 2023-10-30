6171ad5c658389d3a70b384b15b4747e18e14890
Vitor Otsuka <vitor-ots@hotmail.com>
2023-10-30 16:44:18 -0300

    Adicionando rotas nomeadas e filtros com parametros GET

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Category
 # Create your views here.
 
 #MVC - Django
@@ -22,11 +22,21 @@ def home_param(request, post_id):
 def post_list(request):
     #name = 'Vitor'
     #return render(request, 'post_list.html', {'name': 'name'})
-    posts = Post.objects.all()
-    return render(request, 'post_list.html', {'posts': posts})
+    if 'category_id' in request.GET:
+        #category = Category.objects.get(id=request.GET['category_id'])
+        posts = Post.objects.filter(categories=request.GET['category_id'])
+    else:
+        posts = Post.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'post_list.html', {
+        'posts': posts,
+        'categories': categories
+    })
 
 def post_show(request, post_id):
-    #name = 'Vitor'
-    #return render(request, 'post_list.html', {'name': 'name'})
     post = Post.objects.get(id=post_id)
-    return render(request, 'post_show.html', {'post': post})
+    categories = Category.objects.all()
+    return render(request, 'post_show.html', {
+        'post': post,
+        'categories': categories
+    })
